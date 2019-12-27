56fd1aba359fe69a3eb78b5cf662d09e97a3b6db
jijianfeng <1065109432@qq.com>
2019-12-27 22:02:25 +0800

    Initial commit

---
+++ 01-第一个python程序.py
@@ -0,0 +1,21 @@
+#打印一个你好世界
+#
+# """
+# 多行注释
+# 123
+# """
+# print("hello world!!!")
+#
+# age = 15
+# print("小明的年龄是%d岁" % age)
+# age += 2
+# print("小明的年龄是%d岁" % age)
+
+my_name = "小米"
+my_age = 20
+my_height = 190.03
+is_man = True
+print("我拍的名字是：%s" % my_name)
+print("我的年龄是：%d%%岁" % my_age)
+print("我的身高是：%.3f岁" % my_height)
+print("是否是男性：%d" % is_man)
